542811bfd42a966531a5a5d294b8c9e26d2e0407
j3y <40372435+jjszaniszlo@users.noreply.github.com>
2025-07-21 19:23:03 -0400

    feat: Add Boilerplate.  SceneManager, LoadManager, a loading screen and some test scenes. (#1)
    * add GodotUtilities
    
    * feat: Implement LoadManager, a manager for loading resources in the background
    
    * feat: Implement SceneManager, a utility for managing and loading active scenes
    
    * chore: Add SceneManager and LoadManager to autoloads
    
    * chore: Add test scenes for the LoadManager and SceneManager
    
    * feat: Add a basic loading screen to let users know how far along they are in loading progress
    
    * chore: Add a basic UI theme (only for progress bars for now)
    
    * feat: Add a FadeEffectComponent to fade canvas items.

---
+++ Autoloads/LoadManager.cs
@@ -0,0 +1,80 @@
+using System.Threading.Tasks;
+using Godot;
+using Godot.Collections;
+using GodotUtilities;
+
+public partial class LoadManager : Node
+{
+    public static LoadManager Instance { get; private set; }
+    public bool UseSubThreads { get; set; } = false;
+
+    private string _scenePath;
+    private Node _loadInto;
+    private LoadingScreen _loadingScreen;
+
+    [Signal]
+    public delegate void LoadProgressChangedEventHandler(float progress);
+    [Signal]
+    public delegate void LoadDoneEventHandler();
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public async Task LoadScene(string scenePath, LoadingScreen loadingScreen, Node loadInto)
+    {
+        _loadInto = loadInto ?? GetTree().Root;
+        _scenePath = scenePath;
+
+        if (loadingScreen != null)
+        {
+            _loadInto.AddChild(loadingScreen);
+
+            _loadingScreen = loadingScreen;
+
+            LoadProgressChanged += _loadingScreen.UpdateProgressBar;
+            LoadDone += _loadingScreen.LeaveTransition;
+
+            await ToSignal(loadingScreen, LoadingScreen.SignalName.LoadingScreenFullyShown);
+        }
+
+        StartLoad();
+    }
+
+    private void StartLoad()
+    {
+        var resourceThreadRequest = ResourceLoader.LoadThreadedRequest(_scenePath, "", UseSubThreads);
+        if (resourceThreadRequest == Error.Ok)
+        {
+            SetProcess(true);
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        var progress = new Array();
+        var loadStatus = ResourceLoader.LoadThreadedGetStatus(_scenePath, progress);
+        switch (loadStatus)
+        {
+            case ResourceLoader.ThreadLoadStatus.InvalidResource:
+                SetProcess(false);
+                return;
+            case ResourceLoader.ThreadLoadStatus.Failed:
+                SetProcess(false);
+                return;
+            case ResourceLoader.ThreadLoadStatus.InProgress:
+                EmitSignal(SignalName.LoadProgressChanged, progress[0]);
+                break;
+            case ResourceLoader.ThreadLoadStatus.Loaded:
+                var loadedResource = (PackedScene)ResourceLoader.LoadThreadedGet(_scenePath);
+                EmitSignal(SignalName.LoadProgressChanged, progress[0]);
+                EmitSignal(SignalName.LoadDone);
+                LoadProgressChanged -= _loadingScreen.UpdateProgressBar;
+                LoadDone -= _loadingScreen.LeaveTransition;
+                _loadInto.AddChild(loadedResource.Instantiate());
+                break;
+
+        }
+    }
+}
---
+++ Scenes/Scene.cs
@@ -0,0 +1,33 @@
+using Godot;
+using GodotUtilities;
+using System;
+using System.Threading.Tasks;
+
+[Scene]
+public partial class Scene : Node2D
+{
+    [Node]
+    public Button ChangeScene { get; private set; }
+
+    [Export]
+    public string SceneChange { get; private set; }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+
+    public override void _Ready()
+    {
+        ChangeScene.Pressed += OnChangeScenePressed;
+    }
+
+    private async void OnChangeScenePressed()
+    {
+        SceneManager.Instance.LoadingScreen = GD.Load<PackedScene>("res://Scenes/UI/loading_screen.tscn");
+        await SceneManager.Instance.SwapScenes(SceneChange, null, this);
+    }
+}
