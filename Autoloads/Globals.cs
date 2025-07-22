a0bc859f2c3b104a2955cbfc1132037e8e160be2
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-07-22 16:52:25 -0700

    fix!: Fix Pixel scaling by changing to a new method, with a shader required on all sprites.  Remove's SubpixelViewport and all related

--- Autoloads/Globals.cs
+++ Autoloads/Globals.cs
@@ -5,7 +5,6 @@ public partial class Globals : Node
 {
 	public static Globals Instance { get; private set; }
 
-	public ShaderMaterial SubViewportShaderMaterial { get; set; }
 	public Camera2D MainCamera { get; set; }
 
 	public override void _Ready()
--- Scenes/Components/CameraFollowComponent.cs
+++ Scenes/Components/CameraFollowComponent.cs
@@ -5,30 +5,19 @@ public partial class CameraFollowComponent : Camera2D
 	[Export]
 	public CharacterBody2D Player { get; private set; }
 
-	private Vector2 _cameraPosition = Vector2.Zero;
-
 	[Export]
 	public float FollowRate { get; private set; } = 3f;
 
 	public override void _Ready()
 	{
-		_cameraPosition = Player.GlobalPosition;
+		Position = Player.GlobalPosition;
 
 		Globals.Instance.MainCamera = this;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		_cameraPosition = _cameraPosition.Lerp(Player.GlobalPosition, (float)delta * FollowRate);
-
-		var cameraSubpixelOffset = _cameraPosition.Round() - _cameraPosition;
-
-		if (Globals.Instance != null && Globals.Instance.SubViewportShaderMaterial != null)
-		{
-			Globals.Instance.SubViewportShaderMaterial.SetShaderParameter("camera_offset", cameraSubpixelOffset);
-		}
-
-		Position = _cameraPosition.Round();
+		Position = Position.Lerp(Player.GlobalPosition, (float)delta * FollowRate);
 	}
 }
 
