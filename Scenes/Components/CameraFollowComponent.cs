7e8491a726b0279154471ebcfeb51e6d11a20fbf
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-07-22 11:28:03 -0700

    feat: Add a smooth camera follow component

---
+++ Scenes/Components/CameraFollowComponent.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+public partial class CameraFollowComponent : Camera2D
+{
+	[Export]
+	public CharacterBody2D Player { get; private set; }
+
+	private Vector2 _cameraPosition = Vector2.Zero;
+
+	[Export]
+	public float FollowRate { get; private set; } = 3f;
+
+	public override void _Ready()
+	{
+		_cameraPosition = Player.GlobalPosition;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_cameraPosition = _cameraPosition.Lerp(Player.GlobalPosition, (float)delta * FollowRate);
+
+		var cameraSubpixelOffset = _cameraPosition.Round() - _cameraPosition;
+
+		if (Globals.Instance != null && Globals.Instance.SubViewportShaderMaterial != null)
+		{
+			Globals.Instance.SubViewportShaderMaterial.SetShaderParameter("camera_offset", cameraSubpixelOffset);
+		}
+
+		Position = _cameraPosition.Round();
+	}
+}
+
