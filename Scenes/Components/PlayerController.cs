eec4ed07f5daca160413e9dd82e63a23452a7d5c
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-07-22 11:27:44 -0700

    feat: Add Player related Components and Player scene

---
+++ Scenes/Components/PlayerController.cs
@@ -0,0 +1,36 @@
+using Godot;
+using GodotUtilities;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+
+public partial class PlayerController : Node2D
+{
+	[Export]
+	public VelocityComponent VelocityComponent { get; private set; }	
+	public CharacterBody2D Player { get; private set; }
+
+    public override void _Ready()
+    {
+		Player = GetParentOrNull<CharacterBody2D>();
+		if (Player == null)
+		{
+			GD.PrintErr("PlayerController must be a child of a CharacterBody2D node.");
+			return;
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var inputDirection = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+
+		if (inputDirection.LengthSquared() == 0)
+		{
+			VelocityComponent.MinimizeVelocity();
+		}
+		else
+		{
+			VelocityComponent.AccelerateToMaxVelocity(inputDirection);
+		}
+		VelocityComponent.Move(Player);
+	}
+}
+
