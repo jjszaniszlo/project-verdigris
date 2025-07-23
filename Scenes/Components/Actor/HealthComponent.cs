ebcfbac1281aca616d244f5f238db81ad5492451
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-07-23 14:07:31 -0700

    feat!: Make HealthComponent more comprehensive by covering several

--- Scenes/Components/Actor/HealthComponent.cs
+++ Scenes/Components/Actor/HealthComponent.cs
@@ -1,16 +1,21 @@
-using Godot;
 using System;
+using Godot;
 
 namespace Scenes.Components.Actor;
 
 public partial class HealthComponent : Node
 {
-	[Export] public int MaxHealth { get; set; } = 100;
+	[Export]
+	public int MaxHealth { get; private set; } = 100;
+
 	public int CurrentHealth { get; private set; }
 
-	[Signal]
-	public delegate void HealthChangedEventHandler(int current, int max);
+	public bool IsHealthy => CurrentHealth > 0;
+	public bool IsDamaged => CurrentHealth < MaxHealth;
+	public float CurrentHealthRatio => CurrentHealth / MaxHealth;
 
+	[Signal]
+	public delegate void HealthChangedEventHandler(HealthChangedContext healthChangedContext);
 	[Signal]
 	public delegate void DiedEventHandler();
 
@@ -19,10 +24,16 @@ public partial class HealthComponent : Node
 		CurrentHealth = MaxHealth;
 	}
 
-	public void TakeDamage(int amount)
+	public void Damage(int amount)
 	{
 		CurrentHealth = Mathf.Max(CurrentHealth - amount, 0);
-		EmitSignal(SignalName.HealthChanged, CurrentHealth, MaxHealth);
+		EmitSignal(SignalName.HealthChanged, new HealthChangedContext
+		{
+			CurrentHealth = CurrentHealth,
+			MaxHealth = MaxHealth,
+			CurrentHealthRatio = CurrentHealthRatio,
+			IsHeal = false
+		});
 
 		if (CurrentHealth == 0)
 		{
@@ -33,13 +44,45 @@ public partial class HealthComponent : Node
 	public void Heal(int amount)
 	{
 		CurrentHealth = Mathf.Min(CurrentHealth + amount, MaxHealth);
+		EmitSignal(SignalName.HealthChanged, new HealthChangedContext
+		{
+			CurrentHealth = CurrentHealth,
+			MaxHealth = MaxHealth,
+			CurrentHealthRatio = CurrentHealthRatio,
+			IsHeal = true
+		});
+	}
+
+	public partial class HealthChangedContext : Resource {
+		public int CurrentHealth { get; set; }
+		public int MaxHealth { get; set; }
+		public float CurrentHealthRatio { get; set; }
+		public bool IsHeal { get; set; }
+	};
+
+	public void SetMaxHealth(int max, MaxHealthUpdateType mode = MaxHealthUpdateType.KeepCurrentHealth)
+	{
+		switch (mode)
+		{
+			case MaxHealthUpdateType.KeepCurrentHealth:
+				CurrentHealth = Mathf.Min(CurrentHealth, MaxHealth);
+				break;
+			case MaxHealthUpdateType.ResetToMaxHealth:
+				CurrentHealth = max;
+				break;
+			case MaxHealthUpdateType.Scale:
+				CurrentHealth *= max / MaxHealth;
+				break;
+		}
+
+		MaxHealth = max;
 		EmitSignal(SignalName.HealthChanged, CurrentHealth, MaxHealth);
 	}
 
-	public void SetMaxHealth(int max)
+	public enum MaxHealthUpdateType
 	{
-		MaxHealth = max;
-		CurrentHealth = Mathf.Min(CurrentHealth, MaxHealth);
-		EmitSignal(SignalName.HealthChanged, CurrentHealth, MaxHealth);
+		KeepCurrentHealth,
+		ResetToMaxHealth,
+		Scale
 	}
 }
